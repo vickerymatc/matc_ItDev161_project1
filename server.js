30236fe0344671af751511cab5e755c56625f442
Chris Vickery <vickeryc@gmail.com>
2021-10-05 14:03:32 -0500

    added data validation and user model for mongo db

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 import express from "express";
 import connectDB from "./config/db";
+import { check } from "express-validator";
+import { validationResult } from "express-validator";
 
 // init express
 const app = express();
 // connect to the database
 connectDB();
+//config middleWare
+app.use(express.json({ extended: false }));
 
-//end point
+// api endpoints
+//get
 app.get("/", (req, res) =>
   res.send("http get request snd to root api endpoint")
+);
+//post
+app.post(
+  "/api/users",
+  [
+    check("name", "please enter your name").not().isEmpty(),
+    check("email", "please enter a vlaid email").isEmail(),
+    check(
+      "password",
+      "please enter a password with 6 or mor characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    } else {
+      return res.send(req.body);
+    }
+  }
 );
 
 //connect listener
